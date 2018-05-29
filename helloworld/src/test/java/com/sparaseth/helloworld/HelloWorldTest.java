e7068c9e8a4506550e5d96e9dea206e85821b201
sparaseth <sparaseth@gmail.com>
2018-05-29 22:24:01 +0530

    added test method

--- helloworld/src/test/java/com/sparaseth/helloworld/HelloWorldTest.java
+++ helloworld/src/test/java/com/sparaseth/helloworld/HelloWorldTest.java
@@ -1,4 +1,14 @@
 package com.sparaseth.helloworld;
 
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class HelloWorldTest {
+    @Test
+    public void testMain(){
+       assertTrue(true);
+    }
 }
